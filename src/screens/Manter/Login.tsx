5ec1e968ce07698f2bec8fd671ad6f6b18dc24e2
Wesley Silva <ws78009@gmail.com>
2024-09-19 21:36:29 -0300

    :bug: fix correção de caminho

--- src/screens/Manter/Login.tsx
+++ src/screens/Manter/Login.tsx
@@ -204,7 +204,7 @@ export default function Login() {
           </View>
 
           <View style={styles.footer}>
-            <TouchableOpacity style={styles.footerButton} onPress={() => navigation.navigate('Home')}>
+            <TouchableOpacity style={styles.footerButton} onPress={() => navigation.navigate('Cadastro')}>
               <Text style={styles.BtnCadastro}>Cadastro</Text>
             </TouchableOpacity>
           </View>
