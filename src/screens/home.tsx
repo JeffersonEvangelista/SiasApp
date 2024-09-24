d110bec1dfc8d011d8df57226ff8a0c94e4d360b
Weslry Silva <ws78009@gmail.com>
2024-09-24 18:06:07 -0300

    Feat: obtendo o id do usuário
    - mostrando no home só para testar mesmo

--- src/screens/home.tsx
+++ src/screens/home.tsx
@@ -1,11 +1,23 @@
+// Importações necessárias
 import { StatusBar } from "expo-status-bar";
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { getCurrentUserData } from "../services/Firebase"; 
 
 export default function Home() {
+  const userData = getCurrentUserData();
   return (
     <View style={styles.container}>
       <Text>Home</Text>
+      {userData ? ( 
+        <>
+          <Text>Usuário Logado:</Text>
+          <Text>Email: {userData.email}</Text>
+          <Text>ID: {userData.id}</Text>
+        </>
+      ) : (
+        <Text>Nenhum usuário logado.</Text>
+      )}
       <StatusBar style="auto" />
     </View>
   );
--- src/services/Firebase.ts
+++ src/services/Firebase.ts
@@ -1,7 +1,5 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth, createUserWithEmailAndPassword, signOut, getReactNativePersistence, initializeAuth } from 'firebase/auth';
-import { collection, doc, getDoc, setDoc, getFirestore } from 'firebase/firestore';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import { getAuth, createUserWithEmailAndPassword, signOut } from 'firebase/auth';
 
 // Configuração do Firebase
 const firebaseConfig = {
@@ -11,34 +9,18 @@ const firebaseConfig = {
     storageBucket: "siasmobile-5a101.appspot.com",
     messagingSenderId: "18631145700",
     appId: "1:18631145700:web:7c3d01d6c29f52804394f1",
-    measurementId: "G-J29RZYWT9D",
+    measurementId: "G-J29RZYWT9D"
 };
 
 // Inicialize o Firebase
-const app = initializeApp(firebaseConfig)
-const db = getFirestore(app);
-
-export const auth = initializeAuth(app, {
-    persistence: getReactNativePersistence(AsyncStorage)
-})
-
-export const usersRef = collection(db, 'users');
-export const roomRef = collection(db, 'rooms')
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
 // Função para registrar o usuário
-export const registerUser = async (email: string, password: string, username:string, identificador:string) => {
+export const registerUser = async (email: string, password: string) => {
     try {
-        const response = await createUserWithEmailAndPassword(auth, email, password);
-
-        await setDoc(doc(db, "users", response?.user?.uid),{
-            username,
-            email,
-            identificador,
-            userId: response?.user?.uid
-        });
-        return {success: true, data: response?.user}
+        await createUserWithEmailAndPassword(auth, email, password);
         console.log('Usuário registrado com sucesso');
-
     } catch (error) {
         if (error instanceof Error) {
             console.error('Erro ao registrar usuário:', error.message);
@@ -49,21 +31,28 @@ export const registerUser = async (email: string, password: string, username:str
     }
 };
 
+
 // Função para obter o e-mail do usuário logado atualmente
 export const getCurrentUserEmail = (): string | null => {
     const user = auth.currentUser;
     return user ? user.email : null;
 };
 
+// Função para obter os dados do usuário logado atualmente
+export const getCurrentUserData = () => {
+    const user = auth.currentUser;
+    return user ? { email: user.email, id: user.uid } : null;
+};
+
 // Função para realizar o logoff
 export const logOutUser = async () => {
     const auth = getAuth();
     try {
-      await signOut(auth);
-      console.log('Usuário deslogado com sucesso');
-      return true; // Sucesso
+        await signOut(auth);
+        console.log('Usuário deslogado com sucesso');
+        return true; // Sucesso
     } catch (error) {
-      console.error('Erro ao fazer logoff:', error);
-      return false; // Falha
+        console.error('Erro ao fazer logoff:', error);
+        return false; // Falha
     }
-  };
+};
